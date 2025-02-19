305bcb80908b80a9b040921e3bb989b0dc70f4aa
tony chen <tony.zq.chen@gmail.com>
2025-02-19 15:56:06 +0800

    feat: add safeArea

--- harmony/entry/src/main/cpp/PackageProvider.cpp
+++ harmony/entry/src/main/cpp/PackageProvider.cpp
@@ -1,10 +1,12 @@
 #include "RNOH/PackageProvider.h"
 #include "generated/RNOHGeneratedPackage.h"
+#include "SafeAreaViewPackage.h"
 
 using namespace rnoh;
 
 std::vector<std::shared_ptr<Package>> PackageProvider::getPackages(Package::Context ctx) {
     return {
         std::make_shared<RNOHGeneratedPackage>(ctx),
+        std::make_shared<SafeAreaViewPackage>(ctx),
     };
 }
